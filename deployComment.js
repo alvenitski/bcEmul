ab24ef45b8f2096f6c2305cb842867524628dbd1
alvenitski <122977965+alvenitski@users.noreply.github.com>
2023-01-23 17:36:47 +0100

    added Background

--- deployComment.js
+++ deployComment.js
@@ -1,27 +1,31 @@
 //Creation fonction permettant de deployer plus de commentaires//
     //Creation de nouvelles variables avec nouveau contenu :
-    let addComments = '<div id="com4" class="combox">' +
-                        '<div class="picbox">'+
-                            '<img src="./files/comment4.jpg" />'+
-                        '</div>' +
-                        '<p style="color:white"><span>dBask</span> Im a simple man, I see Mindspring Memories, I click buy.' +
-                            'Piste favorite : Safe Inside III.</p>' +
-                    '</div>' + 
-                    '<div id="com5" class="combox">'+
-                        '<div class="picbox">' +
-                            '<img src="./files/comment5.jpg" />' +
-                        '</div>'+
+    let addComments = '<div id="addComs">' +
+                            '<div id="com4" class="combox">' +
+                                '<div class="picbox">'+
+                                    '<img src="./files/comment4.jpg" />'+
+                                '</div>' +
+                                '<p style="color:white"><span>dBask</span> Im a simple man, I see Mindspring Memories, I click buy.' +
+                                    'Piste favorite : Safe Inside III.</p>' +
+                            '</div>' + 
+                            '<div id="com5" class="combox">'+
+                                '<div class="picbox">' +
+                                    '<img src="./files/comment5.jpg" />' +
+                                '</div>'+
 
-                        '<p style="color:white"><span>Cardian Matsu</span> he slush of this EP is undeniable.' +
-                        'really an album to just sit back and float away into hazy vistas of woods and' +
-                            'mountainsides.' +
-                            'Piste favorite : Safe Inside II.</p>' +
+                                '<p style="color:white"><span>Cardian Matsu</span> he slush of this EP is undeniable.' +
+                                'really an album to just sit back and float away into hazy vistas of woods and' +
+                                    'mountainsides.' +
+                                    'Piste favorite : Safe Inside II.</p>' +
+                                '</div>' +
+                                '<a id="remove"> Show less comments</a>'+
                         '</div>';
     //Creation variable eventTarget
     let eventTarget = document.getElementById('deploycom');
 
     //Creation fonction qui permet de deployer nouveaux commentaires
     function addComs(){
-        eventTarget.innerHTML = addComments
+        eventTarget.innerHTML = addComments 
     }
     eventTarget.addEventListener('click', addComs)
+
